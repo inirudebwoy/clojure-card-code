ee3d8e3605c5312bf68509e7200b4a0fa16c3157
Michael Klich <michal@michalklich.com>
2015-09-02 22:18:49 +0100

    Initial commit.

---
+++ test/clojure_card_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-card-code.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-card-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
